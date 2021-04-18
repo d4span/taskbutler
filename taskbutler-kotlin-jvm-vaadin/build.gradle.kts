f80dbb009b02890be904a04658ded6a4635818a1
David Caspar <david.caspar@aol.ch>
2021-04-18 14:31:00 +0200

    Added kotest dependencies.

--- taskbutler-kotlin-jvm-vaadin/build.gradle.kts
+++ taskbutler-kotlin-jvm-vaadin/build.gradle.kts
@@ -1,7 +1,22 @@
 group = "org.taskbutler"
 version = "1.0-SNAPSHOT"
 
+val kotestVersion = "4.4.3"
+
 plugins {
     kotlin("jvm") version "1.4.32"
 }
 
+repositories {
+    mavenCentral()
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+
+dependencies {
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-property:$kotestVersion")
+}
