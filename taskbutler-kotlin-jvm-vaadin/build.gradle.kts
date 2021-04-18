7a2f90f51f9a47b547fa1f219bc324c2b8684bba
David Caspar <david.caspar@aol.ch>
2021-04-18 18:49:14 +0200

    Included Spring Boot and fixed a few things by copying the Vaadin project templates.

--- taskbutler-kotlin-jvm-vaadin/build.gradle.kts
+++ taskbutler-kotlin-jvm-vaadin/build.gradle.kts
@@ -1,12 +1,19 @@
 group = "org.taskbutler"
 version = "1.0-SNAPSHOT"
 
+val kotlinversion = "1.4.32"
 val kotestVersion = "4.4.3"
-val vaadinVersion = "14.+"
+
+val vaadinVersion = "14.5.2"
+
+val springBootVersion = "2.2.4.RELEASE"
+val springDependencyManagementVersion = "1.0.9.RELEASE"
 
 plugins {
     kotlin("jvm") version "1.4.32"
-    id("com.vaadin") version "0.17.0.1"
+
+    id("com.vaadin") version "0.14.5.1"
+    id("org.springframework.boot") version "2.2.4.RELEASE"
 }
 
 repositories {
@@ -20,8 +27,12 @@ tasks.withType<Test> {
 
 
 dependencies {
-    implementation("com.vaadin:vaadin-core:$vaadinVersion")
+    implementation("com.vaadin:vaadin-spring-boot-starter:$vaadinVersion")
 
+    testImplementation("org.springframework.boot:spring-boot-devtools:$springBootVersion")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:$springBootVersion") {
+        exclude("org.junit.vintage", "junit-vintage-engine")
+    }
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-property:$kotestVersion")
 }
