85867826ad7a3ea147592f01e7cabe6e881455d0
David Caspar <david.caspar@aol.ch>
2021-04-18 14:52:29 +0200

    Added Vaadin dependencies.

--- taskbutler-kotlin-jvm-vaadin/build.gradle.kts
+++ taskbutler-kotlin-jvm-vaadin/build.gradle.kts
@@ -2,13 +2,16 @@ group = "org.taskbutler"
 version = "1.0-SNAPSHOT"
 
 val kotestVersion = "4.4.3"
+val vaadinVersion = "14.+"
 
 plugins {
     kotlin("jvm") version "1.4.32"
+    id("com.vaadin") version "0.17.0.1"
 }
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
 tasks.withType<Test> {
@@ -17,6 +20,8 @@ tasks.withType<Test> {
 
 
 dependencies {
+    implementation("com.vaadin:vaadin-core:$vaadinVersion")
+
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-property:$kotestVersion")
 }
